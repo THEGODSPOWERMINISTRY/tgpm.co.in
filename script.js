a2d84395e647beb1ed5d9d585b9653c1266ce050
THEGODSPOWERMINISTRY <thegodspowerministry@gmail.com>
2026-03-29 00:51:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openImg(src){
+  document.getElementById("popup").style.display="flex";
+  document.getElementById("popupImg").src=src;
+}
+function closeImg(){
+  document.getElementById("popup").style.display="none";
+}
